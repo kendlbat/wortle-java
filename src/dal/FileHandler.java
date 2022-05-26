2631aec068cdd18167b0cc73804c526fba0c3cb4
kendlbat <kendlbat@edu.htl-villach.at>
2022-05-26 14:49:30 +0200

    Add wordlist.txt to jar bundle for simplicity

--- src/dal/FileHandler.java
+++ src/dal/FileHandler.java
@@ -1,7 +1,11 @@
 package dal;
 
+import app.Main;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
@@ -16,12 +20,25 @@ public abstract class FileHandler {
         List<String> list = new LinkedList<>();
         String line;
 
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            while ((line = br.readLine()) != null) {
-                list.add(line);
+        // Try loading wordlist.txt from jar
+        InputStream is = Main.class.getClassLoader().getResourceAsStream("wordlist.txt");
+
+        if (is != null) {
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+                while ((line = br.readLine()) != null) {
+                    list.add(line);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } else {
+            try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+                while ((line = br.readLine()) != null) {
+                    list.add(line);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
         return list;
