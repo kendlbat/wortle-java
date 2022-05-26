11bc80120552eb65643dc295259d6891ac09d173
kendlbat <kendlbat@edu.htl-villach.at>
2022-05-26 10:26:52 +0200

    Fix FileHandler only reading first line

--- src/dal/FileHandler.java
+++ src/dal/FileHandler.java
@@ -6,20 +6,15 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
-public class FileHandler {
-    public static String getRandomWord() {
-        Random random = new Random();
-
-        List<String> words = readFileLines("wordlist.txt");
-
-        return words.get(random.nextInt(words.size()));
-    }
-
+public abstract class FileHandler {
     public static List<String> readFileLines(String fileName) {
         List<String> list = new LinkedList<>();
+        String line;
 
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            list.add(br.readLine());
+            while ((line = br.readLine()) != null) {
+                list.add(line);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
